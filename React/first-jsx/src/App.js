2b55546136c0cecd10f228afac117d2ca82ac40e
Sheldon Wolff <sheldonwolff96@gmail.com>
2022-02-10 19:51:00 -0800

    First JSX

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+function App() {
+  return (
+      <>
+        <h1 className="header">Hello Dojo!</h1>
+        <h2 className="header">Things I need to do:</h2>
+        <ul className="list">
+          <li className="list-item">Learn React</li>
+          <li className="list-item">Climb Mt. Everest</li>
+          <li className="list-item">Run a marathon</li>
+          <li className="list-item">Feed the dogs</li>
+        </ul>
+      </>
+  );
+}
+
+
+export default App;
